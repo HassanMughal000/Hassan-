2f87e6dcdf562d98e4eaf060dfb4400eafadf6ea
HAMMAD-ABDUL-RASHEED <hammadabdulrasheed11@gmail.com>
2022-11-24 12:01:05 +0500

    my project on github

---
+++ index1.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Uploading Project On GitHub</title>
+</head>
+<body>
+
+</body>
+</html>
---
+++ index2.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+<h2>Hassan Mughal</h2>
+
+</body>
+</html>
